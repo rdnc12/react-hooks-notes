b6eaf26b4587138e0434e62628e45ff89f398cfd
Erdinc <Erdinc@gklfmg.com>
2020-01-08 12:13:49 +0100

    useRef

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import Card from "../UI/Card";
 import "./Search.css";
@@ -6,28 +6,48 @@ import "./Search.css";
 const Search = React.memo(props => {
   const { onLoadIngredients } = props;
   const [enteredFilter, setEnteredFilter] = useState("");
+  const inputRef = useRef();
 
   useEffect(() => {
-    const query =
-      enteredFilter.length === 0
-        ? ""
-        : `?orderBy="title"&equalTo="${enteredFilter}"`;
-    fetch("https://react-my-burger-f01f7.firebaseio.com/hookssss.json"+query)
-      .then(response => response.json())
-      .then(responseBodyData => {
-        const loadedIngredients = [];
-        for (const key in responseBodyData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseBodyData[key].title,
-            amount: responseBodyData[key].amount
+    // setTimeout waits the enteredFilter to complete in 500ms
+    // then checks the values.
+    const timer = setTimeout(() => {
+      if (enteredFilter === inputRef.current.value) {
+        const query =
+          enteredFilter.length === 0
+            ? ""
+            : `?orderBy="title"&equalTo="${enteredFilter}"`;
+
+        fetch(
+          "https://react-my-burger-f01f7.firebaseio.com/hookssss.json" + query
+        )
+          .then(response => response.json())
+          .then(responseBodyData => {
+            const loadedIngredients = [];
+            for (const key in responseBodyData) {
+              loadedIngredients.push({
+                id: key,
+                title: responseBodyData[key].title,
+                amount: responseBodyData[key].amount
+              });
+            }
+            // in the end we should establish a connection between search and ingredients component to update ingredients
+            onLoadIngredients(loadedIngredients); // functions are objects and behave like any other value in JS.
+            // this line makes infinitive loop when it changes
+            // onLoadIngredients sends a request for every keystroke and it makes server unnecessary busy.
           });
-        }
-        // in the end we should establish a connection between search and ingredients component to update ingredients
-        onLoadIngredients(loadedIngredients); // functions are objects and behave like any other value in JS.
-        // this line makes infinitive loop when it changes
-      });
-  }, [enteredFilter, onLoadIngredients]);
+      }
+    }, 500);
+
+    //the cleanup function runs when the component gets unmounted.
+    // first render return function does not execute but then on the first keystroke we clean up the old effect run a new one on the second keystroke.
+    // we clean up that previous effect run a new one.
+    return ()=>{
+      // we makes only one setTimeout() in the memory.
+      // it makes our app faster.
+      clearTimeout(timer);
+    }
+  }, [enteredFilter, onLoadIngredients, inputRef]);
 
   return (
     <section className="search">
@@ -35,6 +55,7 @@ const Search = React.memo(props => {
         <div className="search-input">
           <label>Filter by Title</label>
           <input
+            ref={inputRef} // established a connection with useRef()
             type="text"
             value={enteredFilter}
             onChange={event => setEnteredFilter(event.target.value)}
